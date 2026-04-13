75e8b162821f5382cf73ba56cb1089785c8868e4
s80220296-max <s80220296@gmail.com>
2026-04-13 22:54:52 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -117,10 +117,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '小狗来信',
+  HEO_SOCIAL_CARD_TITLE_2: '生活不能没有小狗',
+  HEO_SOCIAL_CARD_TITLE_3: '点击观看我的生活',
+  HEO_SOCIAL_CARD_URL: 'https://notion-next-indol-nine-38.vercel.app/',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
